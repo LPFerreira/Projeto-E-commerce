855ae77d75043500dbaf68d2a193101cf6a6c821
LPFerreira <167807097+LPFerreira@users.noreply.github.com>
2025-05-21 19:18:12 +0100

    Update connectDatabase.js

--- Back End/src/database/connectDatabase.js
+++ Back End/src/database/connectDatabase.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 
 async function connectDatabase() {
   await mongoose.connect(
-    'mongodb+srv://luizpcooltra:GE1uj2xP7Ykws0bp@cluster0.dyxt7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+    'mongodb+srv://luizpcooltra:{passaword}.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
   )
 //mongoDB test - users//
 
